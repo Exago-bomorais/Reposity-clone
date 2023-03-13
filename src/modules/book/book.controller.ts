5c93808b9bf845de75026190b2d7954e7dd31ba0
Bruno Oliveira de Morais <exago.bomorais@universal.ad>
2023-03-13 16:44:58 -0300

    Criação CRUD completo com o NestJs e o PrismaIO

---
+++ src/modules/book/book.controller.ts
@@ -0,0 +1,34 @@
+import { Controller } from '@nestjs/common';
+import { Body, Delete, Get, Param, Patch, Post } from '@nestjs/common/decorators';
+import { BookService } from './book.service';
+import { BookDTO } from './dto/create-book.dto';
+
+@Controller('book')
+export class BookController {
+  constructor(private readonly bookService: BookService) {}
+
+  @Post()
+  async create(@Body() data: BookDTO){
+    return this.bookService.create(data)
+  }
+
+  @Get(':id')
+  async show(@Param('id')  id: string){
+    return this.bookService.findOne(id)
+  }
+
+  @Get()
+  async index(@Param('id') id: string){
+    return this.bookService.findAll()
+  }
+
+  @Patch(':id')
+  async update (@Param('id') id: string, @Body() data: BookDTO){
+    return this.bookService.update(id, data)
+  }
+
+  @Delete(':id')
+  async delete (@Param('id') id: string){
+    return this.bookService.delete(id)
+  }
+}
