5b39fd2d9d4858540118004c216b0b61f605b0db
Bruno Oliveira de Morais <exago.bomorais@universal.ad>
2023-03-14 13:03:02 -0300

    Implementação da rota post no author.controller

--- src/modules/author/author.controller.ts
+++ src/modules/author/author.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthorService } from './author.service';
+import { AuthorDto } from './dto/create-author.dto';
 
 @Controller('author')
 export class AuthorController {
   constructor(private readonly authorService: AuthorService) {}
+
+  @Post()
+  async create(@Body() data: AuthorDto) {
+    return this.authorService.create(data);
+  }
 }
